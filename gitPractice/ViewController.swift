f34a2848b1534f3b28a1e9f31cc0a9de44eb5219
Илья Аношин <ilyanoshin@MacBook-ilyanoshin.local>
2022-10-20 19:13:38 +0400

    initial commit

---
+++ gitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitPractice
+//
+//  Created by Илья Аношин on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
